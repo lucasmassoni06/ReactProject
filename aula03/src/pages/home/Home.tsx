1e20bb6976aa89c43e7170030451a69258900121
Lucas Massoni <lucas.masson0307@gmail.com>
2025-09-03 08:29:12 -0300

    react

---
+++ aula03/src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+function Home() {
+  return (
+    <>
+    <div style={{
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center'
+    }}>
+        <div>
+            <div style={{
+                width:'800vw',
+                display:'flex',
+                flexDirection:'column',
+                alignItems:'center'
+            }}>
+                <h2>Seja bem vindo!!!</h2>
+                <p>Expresse aqui seus pensamentos e opiniões</p>
+
+            </div>
+            <div style={{
+                width:'800vw',
+                display:'flex',
+                flexDirection:'column',
+                alignItems:'center'
+            }}>
+                <img src = 'https://i.imgur.com/VpwApCU.png'
+                alt='Imagem da pagina Home'
+                width='400px'
+                />
+            
+    </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
